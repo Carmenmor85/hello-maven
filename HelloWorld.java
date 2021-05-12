afdb59193cd6eb013572ccf810e1d133db3f4455
Carmen <camorg85@gmail.com>
2021-05-12 11:44:03 +0200

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+	public static void main (String[] args){
+	System.out.println("Hello World");
+		}
+	}
+	
+	//Compilare
+	//testare
+	//lo mettiamo in regit sul desktop
+	// ignoro il fichier .class (  lo metto nella lista ignoro che ieri abbiamo creato nel git space)
+	// creare un commit con il messaggio "initial commit"
+	//creo un repo distante: hello-maven e facciamo push nella repo maven
+	
